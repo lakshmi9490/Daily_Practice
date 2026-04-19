e40ce5133559c4d8c92c161e0727185e74f45401
Swetharasi Lakshmi Narasimha <lakshminarasimha021215@gmail.com>
2026-04-19 16:09:15 +0530

    BitwiseOperators (19-04-26)

---
+++ DSA/BitManipulation/BitwiseOperators.java
@@ -0,0 +1,21 @@
+package com.java.bitmanipulation;
+
+public class BitwiseOperators {
+
+	public static void main(String[] args) {
+		int a = 5;
+		int b = 9;
+		int k =-3;
+		System.out.println(a & b);
+		System.out.println(a | b);
+		System.out.println(a ^ b);
+		System.out.println(a << 2);
+		System.out.println(a >> 1);
+		System.out.println(a >>> 1);
+		System.out.println(k >> 1);
+		System.out.println(k >>> 1);
+		System.out.println(~a);
+
+	}
+
+}
