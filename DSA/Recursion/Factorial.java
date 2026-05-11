4baa24d824440b446d2bb55a7835ec57619fd52a
Swetharasi Lakshmi Narasimha <lakshminarasimha021215@gmail.com>
2026-05-11 19:13:46 +0530

    Factorial (11-05-26)

---
+++ DSA/Recursion/Factorial.java
@@ -0,0 +1,21 @@
+package com.java.recursion;
+
+import java.util.Scanner;
+
+public class Factorial {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		System.out.println(factorial(n));
+		sc.close();
+
+	}
+	static int factorial(int n) {
+		if(n==0 || n==1) {
+			return 1;
+		}
+		return n*factorial(n-1);
+	}
+
+}
