25b4834b32a2ebdcd8fa04077033e9e4b78de619
Swetharasi Lakshmi Narasimha <lakshminarasimha021215@gmail.com>
2026-06-10 13:16:26 +0530

    LinkedHashMap (10-06-26)

---
+++ CoreJava/Collections/LinkedHashMapClass.java
@@ -0,0 +1,16 @@
+package com.java.collections;
+
+import java.util.LinkedHashMap;
+
+public class LinkedHashMapClass {
+
+	public static void main(String[] args) {
+		LinkedHashMap lhm =  new LinkedHashMap();
+		lhm.put(18, "Virat");
+		lhm.put(7, "Dhoni");
+		lhm.put(45, "Rohit");
+		
+		System.out.println(lhm);
+	}
+
+}
