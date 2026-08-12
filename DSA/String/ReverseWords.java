d9f4af59d43cd7824a1c9545f4e7d515837140d2
Swetharasi Lakshmi Narasimha <lakshminarasimha021215@gmail.com>
2026-08-12 20:10:05 +0530

    ReverseWords (12-08-26)

---
+++ DSA/String/ReverseWords.java
@@ -0,0 +1,25 @@
+package com.java.string;
+
+public class ReverseWords {
+
+	public static void main(String[] args) {
+		
+		String s = "Java is fun";
+		
+		System.out.println("Reverse words : "+ reverseWords(s));
+	}
+	public static StringBuilder reverseWords(String s) {
+		String [] words = s.trim().split("\\s+");
+		StringBuilder result = new StringBuilder();
+		
+		for(int i = words.length-1; i >= 0 ; i--) {
+			result.append(words[i]);
+			if(i > 0 ) {
+				result.append(" ");
+			}
+		}
+		
+		return result;
+	}
+
+}
