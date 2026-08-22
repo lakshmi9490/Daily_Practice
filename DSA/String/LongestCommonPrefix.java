2270c2692beda2b5b7c9a089c2838a985100cbd4
Swetharasi Lakshmi Narasimha <lakshminarasimha021215@gmail.com>
2026-08-22 16:07:16 +0530

    LongestCommonPrefix (21-08-26)

---
+++ DSA/String/LongestCommonPrefix.java
@@ -0,0 +1,28 @@
+package com.java.string;
+
+public class LongestCommonPrefix {
+
+	public static void main(String[] args) {
+		String words [] = {"flower","flow","flight"};
+		
+		System.out.println(longestCommonPrefix(words));
+	}
+	public static String longestCommonPrefix(String [] str) {
+		if(str == null || str.length == 0) return "";
+		
+		String prefix = str[0];
+		
+		for(int i=1;i<str.length;i++) {
+			
+			while(str[i].indexOf(prefix) !=0) {
+				
+				prefix = prefix.substring(0,prefix.length()-1);
+				
+				if(prefix.isEmpty()) return "";
+			}
+		}
+		return prefix;
+	
+	}
+
+}
